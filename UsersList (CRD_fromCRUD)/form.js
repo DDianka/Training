cc0fb2ddce12bc43dd0894f7ce54c90500846612
Diana <ddianka.1917@gmail.com>
2019-12-10 01:31:36 +0300

    added Validation

--- UsersList (CRD_fromCRUD)/form.js
+++ UsersList (CRD_fromCRUD)/form.js
@@ -3,30 +3,41 @@ const userName = document.querySelector('#inpName');
 const userSurname = document.querySelector('#inpSurname');
 const userPatronymic = document.querySelector('#inpPatronymic');
 const tbody = document.querySelector('tbody');
+
+function isValid(userName, userSurname, userPatronymic) {
+    return userName && userSurname && userPatronymic
+}
+
 // creating table columns
-function createTableRows() {
-    const row = document.createElement("tr");
-    const tdSurname = document.createElement("td");
-    const tdName = document.createElement("td");
-    const tdPatronymic = document.createElement("td");
-    const actionRemove = document.createElement("td");
+function onSubmit() {
+    if (isValid(userName.value, userSurname.value, userPatronymic.value)) {
+        const row = document.createElement("tr");
+        const tdSurname = document.createElement("td");
+        const tdName = document.createElement("td");
+        const tdPatronymic = document.createElement("td");
+        const actionRemove = document.createElement("td");
 
-    tdSurname.innerText = userSurname.value;
-    tdName.innerText = userName.value;
-    tdPatronymic.innerText = userPatronymic.value;
-    actionRemove.innerHTML = "[X]";
+        tdSurname.innerText = userSurname.value;
+        tdName.innerText = userName.value;
+        tdPatronymic.innerText = userPatronymic.value;
+        actionRemove.innerHTML = "[X]";
 
-    actionRemove.classList.add("remover");
+        actionRemove.classList.add("remover");
 
-    row.appendChild(tdSurname);
-    row.appendChild(tdName);
-    row.appendChild(tdPatronymic);
-    row.appendChild(actionRemove);
+        row.appendChild(tdSurname);
+        row.appendChild(tdName);
+        row.appendChild(tdPatronymic);
+        row.appendChild(actionRemove);
 
-    tbody.appendChild(row);
+        tbody.appendChild(row);
+    } else {
+        alert("Заполните пустое поле");
+    }
+
     // actionRemove.addEventListener("click", (event) => {
     //     event.target.parentNode.remove()
     // });
+
 }
 
 tbody.addEventListener('click', ({ target }) => {
@@ -35,4 +46,4 @@ tbody.addEventListener('click', ({ target }) => {
     }
 });
 
-btn.addEventListener("click", createTableRows);
+btn.addEventListener("click", onSubmit);
